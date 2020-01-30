326486aa8235197a54946cd96e9c86d11e38826a
rache <racheljpai@gmail.com>
2020-01-30 14:39:45 -0800

    Created "Create Account" form for our GUI

---
+++ src/main/java/streamingservice/UI/AccountCreator.java
@@ -0,0 +1,23 @@
+package streamingservice.UI;
+
+import javax.swing.*;
+
+public class AccountCreator {
+
+    private JButton signInButton;
+    private JTextPane createAccountTextPane;
+    private JTextArea userName;
+    private JTextField userNameTextField;
+    private JTextArea firstName;
+    private JTextArea lastName;
+    private JTextField firstNameTextField;
+    private JTextField lastNameTextField;
+    private JTextField emailTextField;
+    private JTextArea email;
+    private JTextField reEnterEmailTextField;
+    private JTextArea reEnterTextField;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
