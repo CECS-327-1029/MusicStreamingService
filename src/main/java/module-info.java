d1e02a300357afa2396ecc31d88b6aa19eeb4257
rache <racheljpai@gmail.com>
2020-01-31 08:43:16 -0800

    Created "Login Page" form for our GUI design

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
-module com.register {
+module streamingservice {
     requires javafx.controls;
     requires javazoom;
     requires com.google.gson;
+    requires java.desktop;
     exports streamingservice;
 }
