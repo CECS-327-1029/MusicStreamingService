8e79909dc62e836cc7b8f498207d606651fb17c4
JMVilla24 <32530336+JMVilla24@users.noreply.github.com>
2020-02-03 22:05:26 -0800

    To figure out how to transition between screens, e.g. go from the log in page to the create account page, I combined your forms into one which is found in the MainUI. There you can see they all fall under one JPanel. The MainUI.java file will contain the functionality for all three screens.
    TODO: DESIGN THE USER VIEW SCREEN AND IMPLEMENT IT'S FUNCTIONALITY.

--- src/main/java/streamingservice/App.java
+++ src/main/java/streamingservice/App.java
@@ -1,50 +1,16 @@
 package streamingservice;
 
-import javazoom.jl.decoder.JavaLayerException;
-import javazoom.jl.player.*;
-import streamingservice.UI.AccountCreator;
+
+import streamingservice.UI.MainUI;
 
 import java.io.IOException;
-import java.io.InputStream;
-import javax.swing.*;
 
 public class App {
 
-    private static final int FRAME_WIDTH = 1000;
-    private static final int FRAME_HEIGHT = 700;
-    private static final String MUSIC_FILE_PATH = "resources" + System.getProperty("file.separator") + "music.json";
-    private static final String USER_FILE_PATH = "resources" + System.getProperty("file.separator") + "users.json";
-   
-      /**
-     * Play a given audio file.
-     * @param file Path of the audio file.
-     */
-    void mp3play(String file) {
-        try {
-            // It uses CECS327InputStream as InputStream to play the song 
-             InputStream is = new CECS327InputStream(file);
-             Player mp3player = new Player(is);
-             mp3player.play();
-	     }
-	     catch (JavaLayerException exception) 
-         {
-	       exception.printStackTrace();
-	     }
-         catch (IOException exception)
-         {
-             exception.printStackTrace();
-         }  
-    }
-     
-     
     public static void main(String[] args) throws IOException {
-        //App player = new App();
-        //player.mp3play("resources/imperial.mp3");
-        JFrame creator = new AccountCreator(FRAME_WIDTH, FRAME_HEIGHT, USER_FILE_PATH);
-        creator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        creator.setVisible(true);
-        creator.setResizable(false);
+        MainUI mainUI = new MainUI();
+
     }
- 
+
 }
 
---
+++ src/main/java/streamingservice/UI/MainUI.java
@@ -0,0 +1,219 @@
+package streamingservice.UI;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import streamingservice.music.User;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainUI {
+
+    private static final int FRAME_WIDTH = 800;
+    private static final int FRAME_HEIGHT = 600;
+    private static final String MUSIC_FILE_PATH = "resources" + System.getProperty("file.separator") + "music.json";
+    private static final String USER_FILE_PATH = "resources" + System.getProperty("file.separator") + "users.json";
+
+    private Gson gson;
+    private List<User> users;
+
+    private CardLayout cardLayout = new CardLayout();
+
+    private JPanel root;
+    private JPanel LogIn;
+    private JPanel AccountCreator;
+    private JPanel UserView;
+    private JLabel titleLabel;
+    private JLabel userNameLabel;
+    private JLabel firstNameLabel;
+    private JLabel lastNameLabel;
+    private JLabel emailLabel;
+    private JLabel reEmailLabel;
+    private JTextField userNameTF;
+    private JTextField firstNameTF;
+    private JTextField lastNameTF;
+    private JTextField emailTF;
+    private JTextField reEmailTF;
+    private JLabel userNameErrorLabel;
+    private JLabel firstNameErrorLabel;
+    private JLabel lastNameErrorLabel;
+    private JLabel emailErrorLabel;
+    private JLabel reEmailErrorLabel;
+    private JButton submitButton;
+    private JLabel h;
+    private JButton createAccountBtn;
+
+
+    public MainUI() throws IOException {
+
+        root.setLayout(cardLayout);
+        root.add(LogIn, "Log In");
+        root.add(AccountCreator, "Create Account");
+        root.add(UserView, "User View");
+        cardLayout.show(root, "Log In");
+
+        createAccountBtn.addActionListener(e -> {
+            cardLayout.show(root, "Create Account");
+        });
+
+
+        submitButton.addActionListener(e -> {
+            if (checkIfAllEntriesFilled() && areAllNecessaryEntriesValid()) {
+                createAccount();
+                cardLayout.show(root, "User View");
+            }
+        });
+
+        JFrame mainFrame = new JFrame("Music Streaming Service");
+        mainFrame.add(root);
+        mainFrame.pack();
+        mainFrame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setVisible(true);
+        mainFrame.setResizable(false);
+
+        gson = new Gson();
+        Reader reader = Files.newBufferedReader(Paths.get(USER_FILE_PATH));
+        users = gson.fromJson(reader, new TypeToken<List<User>>() {}.getType());
+
+    }
+
+    private void createAccount() {
+        User newUser = new User(firstNameTF.getText(), lastNameTF.getText(), emailTF.getText(), userNameTF.getText());
+        if (users == null) {
+            users = new ArrayList<>();
+        }
+        users.add(newUser);
+        try {
+            Writer writer = Files.newBufferedWriter(Paths.get(USER_FILE_PATH));
+            gson.toJson(users, writer);
+            writer.close();
+        } catch (IOException e) {
+            showErrorWindow();
+        }
+    }
+
+    private boolean checkIfAllEntriesFilled() {
+        if (userNameTF.getText().trim().toLowerCase().equals("")) {
+            userNameErrorLabel.setVisible(true);
+            userNameErrorLabel.setText("User Name Needed");
+        } else {
+            userNameErrorLabel.setText("");
+            userNameErrorLabel.setVisible(false);
+        }
+
+        if (firstNameTF.getText().trim().equals("")) {
+            firstNameErrorLabel.setVisible(true);
+            firstNameErrorLabel.setText("First Name Needed");
+        } else {
+            firstNameErrorLabel.setText("");
+            firstNameErrorLabel.setVisible(false);
+        }
+
+        if (lastNameTF.getText().trim().equals("")) {
+            lastNameErrorLabel.setVisible(true);
+            lastNameErrorLabel.setText("Last Name Needed");
+        } else {
+            lastNameErrorLabel.setText("");
+            lastNameErrorLabel.setVisible(false);
+        }
+
+        if (emailTF.getText().trim().toLowerCase().equals("")) {
+            emailErrorLabel.setVisible(true);
+            emailErrorLabel.setText("Email Needed");
+        } else {
+            emailErrorLabel.setText("");
+            emailErrorLabel.setVisible(false);
+        }
+
+        if (reEmailTF.getText().trim().toLowerCase().equals("")) {
+            reEmailErrorLabel.setVisible(true);
+            reEmailErrorLabel.setText("Email Re-entry Needed");
+        } else {
+            reEmailErrorLabel.setText("");
+            reEmailErrorLabel.setVisible(false);
+        }
+
+        JLabel[] labels = {userNameErrorLabel, firstNameErrorLabel, lastNameErrorLabel, emailErrorLabel, reEmailErrorLabel};
+        boolean anyVisible = false;
+        int i = 0;
+        while (!anyVisible && i < labels.length) {
+            anyVisible = labels[i++].isVisible();
+        }
+        return !anyVisible;
+    }
+
+    private boolean areAllNecessaryEntriesValid() {
+        return isUserNameFreeToUse() && areEmailsTheSame() && isEmailFreeToUse();
+    }
+
+    private boolean isUserNameFreeToUse() {
+        boolean isFreeToUse = true;
+        if (users != null) {
+            for (int i = 0; i < users.size() && isFreeToUse; i++) {
+                if (users.get(i).getUserName().equals(userNameTF.getText())) {
+                    isFreeToUse = false;
+                }
+            }
+        }
+        if (!isFreeToUse) {
+            userNameErrorLabel.setText("User Name already In Use");
+            userNameErrorLabel.setVisible(true);
+        } else {
+            userNameErrorLabel.setText("");
+            userNameErrorLabel.setVisible(false);
+        }
+        return isFreeToUse;
+    }
+
+    private boolean isEmailFreeToUse() {
+        boolean isFreeToUse = true;
+        if (users != null) {
+            for (int i = 0; i < users.size() && isFreeToUse; i++) {
+                if (users.get(i).getEmail().equals(userNameTF.getText())) {
+                    isFreeToUse = false;
+                }
+            }
+        }
+
+
+        if (isFreeToUse) {
+            emailErrorLabel.setText("Email already In Use");
+            emailErrorLabel.setVisible(true);
+        } else {
+            emailErrorLabel.setText("");
+            emailErrorLabel.setVisible(false);
+        }
+        return isFreeToUse;
+    }
+
+    private boolean areEmailsTheSame() {
+        boolean isSame = emailTF.getText().equals(reEmailTF.getText());
+        if (!isSame) {
+            reEmailErrorLabel.setText("Emails are different");
+            reEmailErrorLabel.setVisible(true);
+        } else {
+            reEmailErrorLabel.setText("");
+            reEmailErrorLabel.setVisible(false);
+        }
+        return isSame;
+    }
+
+    private void showErrorWindow() {
+        JFrame frame = new JFrame();
+        frame.add(new JPanel().add(new JLabel("Something went wrong. Try Again Later.")));
+        frame.setSize(200, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setResizable(false);
+    }
+
+}
