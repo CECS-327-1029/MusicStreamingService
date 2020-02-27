df0afb9c355822309f274e26384ef49918377f97
pairachel <racheljpai@gmail.com>
2020-02-27 14:14:16 -0800

    Adding ProxyInterface and Catalog (json file that will hold the methods and details of those methods).

---
+++ src/main/java/streamingservice/serverside/ProxyInterface.java
@@ -0,0 +1,17 @@
+package streamingservice.serverside;
+
+import com.google.gson.JsonObject;
+
+public class ProxyInterface {
+
+    JsonObject synchExecution(String remoteMethod, Object... param)
+    {
+        for (String arg : args)
+        {
+            System.out.println(arg);
+        }
+        return null;
+    }
+    
+
+}
